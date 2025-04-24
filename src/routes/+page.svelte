71863b2b2e70983cbcfbf9cdc7004550c97f532d
xarmian <xarmian@gmail.com>
2025-04-23 20:23:49 -0400

    temp download link for 0.1.4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -135,7 +135,7 @@
         
         <!-- Direct APK Download -->
         <div class="mt-8 w-full max-w-md">
-          <a href="https://download.getvera.app/vera-wallet-beta-0.1.4.apk" class="btn btn-primary w-full flex items-center justify-center py-3 px-4 rounded-lg shadow-md">
+          <a href="https://api.voirewards.com/vera-wallet-beta-0.1.4.apk" class="btn btn-primary w-full flex items-center justify-center py-3 px-4 rounded-lg shadow-md">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
             </svg>
